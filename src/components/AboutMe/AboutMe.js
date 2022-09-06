11548ea5e482f8e320d7c8df667f98b7102f43ea
FranciscoCampos <francisco.campos.amoedo@gmail.com>
2022-09-06 17:52:42 -0300

    Section About

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,8 +4,23 @@ const AboutMe = () => {
     return (
         <section id="about">
             <img className="bgAbout" src="../images/background/BgAbout.png" alt="" />
+            <div className="container">
+                <h2>Acerca de mi</h2>
+                <hr />
+                <div className="container-about">
+                    <img src="../images/pictures/DeveloperPopUps.png" alt="" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                        nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
+                        wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+                        lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                        dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
+                        dolore eu feugiat nulla facilisis at vero eros et accum.
+                    </p>
+                </div>
+            </div>
         </section>
-    )
-}
+    );
+};
 
 export default AboutMe;
