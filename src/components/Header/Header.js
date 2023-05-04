996ec53d277b190759525bdd1e4ec2c9b69f55ba
FranciscoCampos <francisco.campos.amoedo@gmail.com>
2023-05-04 13:36:57 -0300

    Puliendo el Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,7 @@ import Home from "../Home/Home";
 const Header = () => {
     return (
         <>
-            {/* <img className="bg-section" src="../images/background/Recurso 4.png" alt="" /> */}
             <header className="header">
-                {/* <img className="bg-section" src="../images/background/BgHome.png" alt="" /> */}
                 <Navbar />
                 <Home />
             </header>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,87 @@
+import { useState, useEffect } from "react";
+import BurgerButton from "../BurgerButton/BurgerButton";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
+    useEffect(() => {
+        function handleResize() {
+          if (window.innerWidth > 768) {
+            setClicked(false);
+          }
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
     return (
-        <div className="container">
-            <div className="row">
-                <nav className="navbar navbar-expand-md navbar-light">
-                    <div className="container">
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavAltMarkup"
-                            aria-controls="navbarNavAltMarkup"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
+        <div className="container-xxl">
+            <nav>
+                <ul className={`nav-links ${clicked ? "active" : ""}`}>
+                    <li>
+                        <a href="#home" className="link-hover" onClick={handleClick}>
+                            INICIO
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about" className="link-hover" onClick={handleClick}>
+                            ACERCA
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#skills" className="link-hover" onClick={handleClick}>
+                            SKILLS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projects" className="link-hover" onClick={handleClick}>
+                            PROYECTOS
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" className="link-hover" onClick={handleClick}>
+                            CONTACTO
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://drive.google.com/file/d/1khzbNfIb0s90MncbUNcnb-IB_85HbkFC/view?usp=share_link"
+                            className="link-hover"
+                            target="_blank"
+                            rel="noreferrer"
                         >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <ul className="navbar-nav">
-                                <a className="nav-link active" aria-current="page" href="#home">
-                                    INICIO
-                                </a>
-                                <a className="nav-link" href="#about">
-                                    ACERCA
-                                </a>
-                                <a className="nav-link" href="#skills">
-                                    SKILLS
-                                </a>
-                                <a className="nav-link" href="#projects">
-                                    PROYECTOS
-                                </a>
-                                <a className="nav-link" href="#contact">
-                                    CONTACTO
-                                </a>
-                                <a className="nav-link" href="#descargar">
-                                    CV
-                                </a>
-                            </ul>
-                            <div className="social-container">
-                                <a className="social" href="https://www.linkedin.com/in/franciscocamposamoedo/" target="_blank" rel="noreferrer">
-                                    <img src="../images/icons/LinkedIn.png" alt="" />
-                                </a>
-                                <a className="social" href="https://github.com/FranCamposAmoedo" target="_blank" rel="noreferrer">
-                                    <img src="../images/icons/GitHub1.png" alt="" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+                            CV
+                        </a>
+                    </li>
+                </ul>
+                <div className="social-container">
+                    <a
+                        className="social"
+                        href="https://www.linkedin.com/in/franciscocamposamoedo/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="../images/icons/LinkedIn.png" alt="" />
+                    </a>
+                    <a
+                        className="social"
+                        href="https://github.com/FranCamposAmoedo"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="../images/icons/GitHub1.png" alt="" />
+                    </a>
+                </div>
+                <div className="burger-container">
+                    <BurgerButton clicked={clicked} handleClick={handleClick} />
+                </div>
+                <div className={`burger-window ${clicked ? "active" : ""}`}></div>
+            </nav>
         </div>
     );
 };
